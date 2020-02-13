0786afdfe3d5863a350a649107cb3a25675e3a0c
Logan Duffley <Lduffley12@gmail.com>
2020-02-13 15:48:49 -0600

    Added Function to Search NASA API

--- nasa-photo-of-day/src/App.js
+++ nasa-photo-of-day/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import DateInput from "./DateInput"
+import Photo from "./Photo"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Nasa Photo of Day</h1>
-    </div>
-  );
+class App extends Component {
+  state={
+    date: "",
+    photo: ""
+  }
+  changeDate = e =>{
+    e.preventDefault()
+    let dataFromInput = e.target[0].value
+    this.setState({date: dataFromInput})
+    this.getPhoto(dataFromInput)
+  }
+
+ getPhoto = date =>(
+   fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=DEMO_KEY`)
+   .then(response => response.json())
+   .then( photoData => this.setState({photo : photoData}))
+ )
+ 
+  render(){
+    return(
+      <div>
+        <h1>Nasa's Astronomy Picture of the Day</h1>
+        <DateInput changeDate={this.changeDate}/>
+        <Photo photo={this.state.photo}/>
+      </div>
+    )
+  }
 }
 
 export default App;
